732637e3c9c16f527f73f16618de752ea09d9b60
stephen <kernan.software@gmail.com>
2023-08-14 20:46:37 -0500

    LTC-70 -- Merge conflicts

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,7 @@
 'use client'
 import React from 'react'
+import { NavBar } from '@/components/NavBar/NavBar'
+import { HeroSection } from '@/components/HeroSection/HeroSection'
 
 const DashboardPage = (): JSX.Element => {
   return (
