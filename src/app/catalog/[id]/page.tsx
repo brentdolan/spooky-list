b07e5f6f146fe2f2e94aa90641d81a9785916c87
Brent Dolan <94938468+brentdolan@users.noreply.github.com>
2023-08-19 14:55:36 -0500

    LTC-81: Made tooltips

--- src/app/catalog/[id]/page.tsx
+++ src/app/catalog/[id]/page.tsx
@@ -4,6 +4,7 @@ import { NavBar } from '@/components/NavBar/NavBar'
 import styles from './movieDetailPage.module.scss'
 import { Button } from '@/components/Button/Button'
 import { BookmarkOutlined, PlaylistAdd } from '@mui/icons-material'
+import { IconButton, Tooltip } from '@mui/material'
 
 const movie = {
   url: 'http://localhost:8000/movies/5/',
@@ -38,12 +39,16 @@ const Page: React.FC = () => {
                   </div>
                   <div className={styles.buttons}>
                       <Button variant={'secondary'} onClick={() => {}}>Mark as Watched</Button>
-                      <Button variant={'minimal'} onClick={() => {}}>
-                          <BookmarkOutlined/>
-                      </Button>
-                      <Button variant={'minimal'} onClick={() => {}}>
-                          <PlaylistAdd/>
-                      </Button>
+                      <Tooltip title={'Bookmark'}>
+                          <IconButton>
+                              <BookmarkOutlined sx={{ fontSize: '2rem' }}/>
+                          </IconButton>
+                      </Tooltip>
+                      <Tooltip title={'Add to List'}>
+                          <IconButton>
+                              <PlaylistAdd sx={{ fontSize: '2rem' }}/>
+                          </IconButton>
+                      </Tooltip>
                   </div>
 
                   <p>{movie.description}</p>
