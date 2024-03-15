158375743657ebb10e8ddfe7024b35a6703178de
Brent Dolan <94938468+brentdolan@users.noreply.github.com>
2024-03-14 20:00:03 -0500

    fixed site title

--- src/components/Form/InputLabel.tsx
+++
@@ -1,9 +0,0 @@
-import { styled } from '@mui/system'
-import { InputLabel as MuiInputLabel } from '@mui/material'
-
-export const InputLabel = styled(MuiInputLabel)({
-  color: 'var(--ghost)',
-  fontFamily: 'var(--primaryFont)',
-  fontWeight: 500,
-  marginBottom: '0.5rem'
-})
